3333894b46c12fca52c47565bbe09c9a8a6ecdd3
Alex Titov <cloud9.alex.tch@gmail.com>
2023-02-16 21:33:54 +0300

    fix: исправить все блоки без отзывчивой верстки

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,23 @@
 const profilePopup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-icon');
-
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
-function openPopup() {
-    profilePopup.classList.add('popup_opened');
-}
-function closePopup() {
-    profilePopup.classList.remove('popup_opened');
-}
-
+const closeButton = document.querySelector('.popup__close-button');
 let profileEditor = document.querySelector('.popup__container');
 let userNameInput = document.querySelector('.popup__data_type_name');
 let userDescriptionInput = document.querySelector('.popup__data_type_description');
 let userName = document.querySelector('.profile__name');
 let userDescription = document.querySelector('.profile__description');
 
-userNameInput.value = userName.textContent;
-userDescriptionInput.value = userDescription.textContent;
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+function openPopup() {
+    userNameInput.value = userName.textContent;
+    userDescriptionInput.value = userDescription.textContent;
+    profilePopup.classList.add('popup_opened');
+}
+function closePopup() {
+    profilePopup.classList.remove('popup_opened');
+}
 
 profileEditor.addEventListener('submit', saveData);
 
