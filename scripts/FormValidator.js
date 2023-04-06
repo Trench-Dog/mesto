a7f3bdc66c2d4339000e2029e7cd4c3e66846df3
Alex Titov <cloud9.alex.tch@gmail.com>
2023-04-06 14:43:24 +0300

    feat: задать класс валидации

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,3 +1,57 @@
 export default class FormValidator {
-    constructor(config, formName) {}
+    constructor(config, formName) {
+        this._form = document.getElementByName(formName)[0];
+        this._formSelector = config.formSelector;
+        this._inputSelector = config.inputSelector;
+        this._submitButtonSelector = config.submitButtonSelector;
+        this._inactiveButtonClass = config.inactiveButtonClass;
+        this._templateErrorClass = config.templateErrorClass;
+        this._incorrectInputClass = config.incorrectInputClass;
+        this._inputList = this._form.querySelectorAll(this._inputSelector);
+    }
+    _toggleButton(submitButton, inputSelector, inactiveButtonClass, form) {
+        const formInputs = Array.from(form.querySelectorAll(inputSelector));
+        const invalidInput = formInputs.some(input => {
+            return !input.validity.valid;
+        });
+        if (invalidInput) {
+            _blockSubmitButton(submitButton, inactiveButtonClass);
+        } else {
+            _unblockSubmitButton(submitButton, inactiveButtonClass);
+        }
+    }
+    _showInputError(errorMessage, errorText, incorrectInputClass) {
+        errorMessage.textContent = errorText;
+        errorMessage.classList.add(incorrectInputClass);
+    }
+    _hideInputError(errorMessage, incorrectInputClass) {
+        errorMessage.textContent = '';
+        errorMessage.classList.remove(incorrectInputClass);
+    }
+    _blockSubmitButton(submitButton, inactiveButtonClass) {
+        submitButton.classList.add(inactiveButtonClass);
+        submitButton.disabled = true;
+    }
+    _unblockSubmitButton(submitButton, inactiveButtonClass) {
+        submitButton.classList.remove(inactiveButtonClass);
+        submitButton.disabled = false;
+    }
+    _checkValidity(input, templateErrorClass, incorrectInputClass) {
+        const errorMessage = document.querySelector(`${templateErrorClass}${input.name}`);
+        if (!input.validity.valid) {
+            _showInputError(errorMessage, input.validationMessage, incorrectInputClass);
+        } else {
+            _hideInputError(errorMessage, incorrectInputClass);
+        }
+    }
+    enableValidation() {
+        this._inputList.forEach(input => {
+            input.addEventListener('input', () => {
+                const form = input.closest(this._formSelector);
+                const submitButton = form.querySelector(this._submitButtonSelector);
+                _checkValidity(input, templateErrorClass, incorrectInputClass);
+                _toggleButton(submitButton, inputSelector, inactiveButtonClass, form);
+            });
+        });
+    }
 }
