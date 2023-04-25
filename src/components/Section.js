6d6bc30fd883094b62b910e2bf567d29c11ebdb7
Alex Titov <cloud9.alex.tch@gmail.com>
2023-04-25 23:14:36 +0300

    refactor: начать формирование секции

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,3 +1,13 @@
 export default class Section {
-    constructor({ items, renderer }) {}
+    constructor({ items, renderer }, containerSelector) {
+        this._initialCards = items;
+        this._renderer = renderer;
+        this._containerSelector = containerSelector;
+    }
+    renderInitialCards() {
+        this._initialCards.forEach(() => {
+            this._renderer;
+        });
+    }
+    addItem() {}
 }
